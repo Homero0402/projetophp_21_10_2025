e22d1d7661a8261c3dc1055f2b9c512f7eda26f2
Homero0402 <homerolinakis09@gmail.com>
2025-10-21 22:08:36 -0300

    formulario de cadastro

---
+++ include/cadastrar.php
@@ -0,0 +1,13 @@
+
+ <?php 
+
+ $primeiroNome = $_POST['primeiroNome'];
+ $segundoNome = $_POST['segundoNome'];
+ $escolhaPets = $_POST['escolhaPets'];
+
+ echo ($primeiroNome . " - " . $segundoNome . " - " . $escolhaPets);
+
+ echo '</br> <a href="..href=/index.php">Voltar</a>';
+
+ //header("location: ..index.php")
+  ?>
